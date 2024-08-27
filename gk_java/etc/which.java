05df15335323dcfb464d8d7163c0b7d2d6e169ef
rokloc <roku.trlt@gmail.com>
2024-08-27 23:01:31 +0900

    which_ok

--- gk_java/etc/which.java
+++ gk_java/etc/which.java
@@ -1,27 +1,28 @@
-puublic class which {
+public class which {
 	public static void main(String[] args) {
 		System.out.println("今日の運勢は");
 		int x;
 		x = new java.util.Random().nextInt(5);
-		swich (x)
+		switch (x){
 		case 0:
-			system.out.println("大吉");
+			System.out.println("大吉");
 			break;
 		case 1:
-			system.out.println("中吉");
+			System.out.println("中吉");
 			break;
 		case 2:
-			system.out.println("小吉");
+			System.out.println("小吉");
 			break;
 		case 3:
-			system.out.println("吉");
+			System.out.println("吉");
 			break;
 		case 4:
-			system.out.println("凶");
+			System.out.println("凶");
 			break;
 		case 5:
-			system.out.println("大凶");
+			System.out.println("大凶");
 			break;
+		}
 	}
 
 }
