45b487c61677529bf9d637a54fce38fc3ca1a229
rokloc <roku.trlt@gmail.com>
2024-08-30 01:24:29 +0900

    constracta

--- gk_java/etc/9_1/Thief.java
+++ gk_java/etc/9_1/Thief.java
@@ -8,4 +8,15 @@ public class Thief {
 		this.hp = hp;
 		this.mp = mp;
 	}
+
+	public Thief(String name, int hp) {
+		this(name, hp, 5);
+	}
+
+	public Thief(String name) {
+		this(name, 40, 5);
+	}
+	
+	public Thief(String name) {
+	}
 }
