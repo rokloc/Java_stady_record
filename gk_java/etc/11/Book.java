9397031ffd92b9564da323365f919553c5a7d43c
rokloc <roku.trlt@gmail.com>
2024-08-30 15:41:05 +0900

    abstract

---
+++ gk_java/etc/11/Book.java
@@ -0,0 +1,58 @@
+public abstract class TangibleAsset {
+	String name;
+	int price;
+	String color;
+	//コンストラクタ
+	public TangibleAsset(String name, int price, String color, String isbn) {
+		this.name = name;
+		this.price = price;
+		this.color = color;
+	}
+
+	//メソッド
+	public String getName()
+		return (this.name);
+	public int getPrice()
+		return (this.price);
+	public String getColor()
+		return (this.color);	
+}
+
+
+public class Book {
+	String isbn;
+	//コンストラクタ
+	public Book(String name, int price, String color, String isbn) {
+		super(name, price, color);
+		this.isbn = isbn;
+	}
+	//メソッド
+	public String getIsbn()
+		return (this.isbn);
+
+}
+
+public class Computer {
+	String makerName;
+	//コンストラクタ
+	public Computer(String name, int price, String color, String makerName) {
+		super(name, price, color);
+		this.makerName = makerName;
+	}
+	//メソッド
+	public String getmakerName()
+		return (this.makerName);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
