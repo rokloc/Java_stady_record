d09054d2b998f7c34fb54aafb3a87c86ffe3ad8a
rokloc <roku.trlt@gmail.com>
2024-08-30 18:51:30 +0900

    polymorphism

---
+++ gk_java/etc/12/Main.java
@@ -0,0 +1,15 @@
+puvlic class Main {
+	public static void main(String[] args) {
+		Y[] array = new Y[2];
+		Y[0] = Y fizz;
+		Y[1] = Y buzz;
+		fizz = new A();
+		buzz = new B();
+
+		for (Y y : array)
+			y.b();
+
+
+	
+	}
+}
