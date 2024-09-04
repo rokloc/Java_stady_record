1247f3c01127b64230e616cc77a5ce39e4389819
rokloc <roku.trlt@gmail.com>
2024-09-04 15:03:17 +0900

    Application_pragram

--- Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,13 +1,33 @@
 package com.example.sample1app;
 
+import java.util.Arrays;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SampleBootApp1Application {
+public class SampleBootApp1Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleBootApp1Application.class, args);
+		//SpringApplication.run(SampleBootApp1Application.class, args);
+		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
 	}
+	
+	@Override
+	public void run(ApplicationArguments args) {
+		System.out.println("+--------------------------------------------+");
+		System.out.println("|   this is Application Runner program.    |");
+		System.out.println("+--------------------------------------------+");
+		System.out.println(args.getOptionNames());
+		System.out.println(args.getNonOptionArgs());
+		System.out.println(Arrays.asList(args.getSourceArgs()));
 
+		
+	}
 }
