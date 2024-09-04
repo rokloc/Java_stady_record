304a695e6e9fd7e4ab46297f8351f4d49b22a0bb
rokloc <roku.trlt@gmail.com>
2024-09-04 16:26:02 +0900

    UI

--- Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,6 +1,9 @@
 package com.example.sample1app;
 
+//javax.swingパッケージ：Javaの標準ライブラリ。GUIを構成するためのクラスが内蔵。
+//JFrameクラス: トップレベルのウィンドウ。これを土台にして、ラベルやボタン、テキストフィールドが配置される。
 import javax.swing.JFrame;
+//swingライブラリに含まれるクラス。テキストや画像など静的な情報を表示するコンポーネント。
 import javax.swing.JLabel;
 
 import org.springframework.boot.ApplicationArguments;
@@ -18,15 +21,21 @@ public class SampleBootApp1Application implements ApplicationRunner {
 		app.setBannerMode(Mode.OFF);
 		//デフォルトはTrue.falseにすると実行時にUIウィンドウが出てくる。
 		app.setHeadless(false);
+		//app実行
 		app.run(args);
 	}
 	
 	@Override
 	public void run(ApplicationArguments args) {
+		//「Spring Boot Swing App」というタイトルの新しいウィンドウを作成します
 		JFrame frame = new JFrame("Spring Boot Swing App");
+		//ウィンドウを閉じたらプログラム終了
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//ウィンドウのサイズ指定
 		frame.setSize(300,200);
+		//新しいラベル追加（中身はSpring Boot Application.）
 		frame.add(new JLabel("Spring Boot Application."));
+		//ウィンドウ表示
 		frame.setVisible(true);
 		/*
 		System.out.println("+--------------------------------------------+");
