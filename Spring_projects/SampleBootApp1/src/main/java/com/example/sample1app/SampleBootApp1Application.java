d81b0ea60f917b57d617599dbb822782c8ce328c
rokloc <roku.trlt@gmail.com>
2024-09-06 13:06:40 +0900

    Rest

--- Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,50 +1,38 @@
 package com.example.sample1app;
 
-//javax.swingパッケージ：Javaの標準ライブラリ。GUIを構成するためのクラスが内蔵。
-//JFrameクラス: トップレベルのウィンドウ。これを土台にして、ラベルやボタン、テキストフィールドが配置される。
-import javax.swing.JFrame;
-//swingライブラリに含まれるクラス。テキストや画像など静的な情報を表示するコンポーネント。
-import javax.swing.JLabel;
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+//import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
-public class SampleBootApp1Application implements ApplicationRunner {
+@RestController
+public class SampleBootApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
-		app.setBannerMode(Mode.OFF);
-		//デフォルトはTrue.falseにすると実行時にUIウィンドウが出てくる。
-		app.setHeadless(false);
-		//app実行
-		app.run(args);
+		SpringApplication.run(SampleBootApp1Application.class, args);
 	}
 	
-	@Override
-	public void run(ApplicationArguments args) {
-		//「Spring Boot Swing App」というタイトルの新しいウィンドウを作成します
-		JFrame frame = new JFrame("Spring Boot Swing App");
-		//ウィンドウを閉じたらプログラム終了
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//ウィンドウのサイズ指定
-		frame.setSize(300,200);
-		//新しいラベル追加（中身はSpring Boot Application.）
-		frame.add(new JLabel("Spring Boot Application."));
-		//ウィンドウ表示
-		frame.setVisible(true);
-		/*
-		System.out.println("+--------------------------------------------+");
-		System.out.println("|   this is Application Runner program.    |");
-		System.out.println("+--------------------------------------------+");
-		System.out.println(args.getOptionNames());
-		System.out.println(args.getNonOptionArgs());
-		System.out.println(Arrays.asList(args.getSourceArgs()));
-
-		*/
+	@RequestMapping("/{num}")
+	public String index(@PathVariable("num") int num) {
+	    if (num == 1) {
+	        return "Hello, ONE!";
+	    }
+	    return "Hello, Spring Boot 3!!";
 	}
+
+	/*
+	//「/」にアクセスしたらindexクラスが呼ばれる
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		if (num == 1)
+			return ("Hello,ONE!");
+		return ("Hello, Spring Boot 3!!");
+	}
+	*/
 }
