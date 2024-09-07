943691688009f895e557d6b8bf6d4d9c8c44e84d
rokloc <roku.trlt@gmail.com>
2024-09-07 16:16:28 +0900

    動的コンテンツ

--- Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ Spring_projects/SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -1,4 +1,28 @@
+//テンプレートエンジン　Thymeleaf
+
+package com.example.sample1app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class HelloController {
+	@RequestMapping()
+	public String index(Model model) {
+	  model.addAttribute("msg", "これはコントローラーに用意したメッセージです。これが動的なコンテンツです");
+	  return "index";
+	}
+}
+
+
+
+
+
 //Controller(RESTでない普通の）
+/*
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
@@ -27,3 +51,4 @@ public class HelloController {
 		}
 
 }
+*/
